9a26448aeb322fdc21a7b91a53bb0cd74aefcb19
Shubham Chaudhary <sc3617378@gmail.com>
2025-09-01 18:04:53 +0530

    Update src/app/layout.tsx only

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,7 +93,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* </UserProvider> */}
 
         {/* ✅ Vercel Web Analytics */}
-        {/* <Analytics /> */}
+        <Analytics />
       </body>
     </html>
   );
